492c8841d032ef359c69cb2e20620b451a2cf4e0
Jotas <jproodian721@gmail.com>
2022-02-03 16:49:58 -0600

    updated candidate routes to join with party data

--- server.js
+++ server.js
@@ -30,7 +30,11 @@ app.get('/', (req, res) => {
 
 // GET candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
     
     db.query(sql, (err, rows) => {
         if (err) {
@@ -107,22 +111,6 @@ app.post('/api/candidates', ({ body }, res) => {
         });
     });
 });
-
-
-
-
-
-// // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES(?,?,?,?)`;
-
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
